f558cdc55fdf200ba2f6357c87a38133339db2ce
Adriel Zanatta <zanattaadriel@gmail.com>
2024-03-03 12:11:49 -0300

    update sqlite

--- test.py
+++
@@ -1,8 +0,0 @@
-qs = [
-    {"id": 13, "winner_team": 0},
-    {"id": 14, "winner_team": 2},
-    {"id": 15, "winner_team": 2},
-    {"id": 16, "winner_team": 1},
-]
-
-print([x["winner_team"] for x in qs if x["id"] == 13][0])
